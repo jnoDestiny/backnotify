bf9d8ac8303b1959d941f57d13025808aff63640
jnoDestiny <thanscot@gmail.com>
2024-05-18 23:19:21 +0300

    WIP, bascially wrote a bunch of non-functioning code

--- backnotify/foo.py
+++ backnotify/foo.py
@@ -1,17 +1,25 @@
-def foo() -> str:
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        foo (str): Description of arg1
-
-    Returns:
-        str: Description of return value
-    """
-
-    return "foo"
+from celery import PeriodicTask
+from datetime import timedelta
+from functools import partial
+from win10toast import ToastNotifier
+from typing import Callable
 
 
-if __name__ == "__main__":  # pragma: no cover
-    pass
+class PeriodicNotification(PeriodicTask):
+    def __init__(self, notification_handler: Callable, period: timedelta):
+        super().__init__()
+        self._period: timedelta = period
+        self._notification_handler: Callable = notification_handler
+
+    def run(self):
+        self._notification_handler()
+
+
+def main():
+    notifier = ToastNotifier()
+    notification_routine = partial(notifier.show_toast, "Straighten your back!", "You can't allow yourself to sit crooked", duration=10, threaded=True)
+    notification_scheduler = PeriodicNotification(notification_routine, timedelta(seconds=20))
+
+
+if __name__ == "__main__":
+    main()
